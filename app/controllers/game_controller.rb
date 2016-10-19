24bdcc5ab99a972dc816e56b8fcfdfa3eb540582
Adam <alk5198@gmail.com>
2016-10-19 17:15:40 -0500

    Updated
    Updated

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,29 @@
+class GameController < ApplicationController
+  def user_plays_rock
+
+ @moves = ["Rock","Paper","Scissors"]
+
+
+
+ @comp_move = @moves.sample
+
+    render("game/play_rock.html.erb")
+
+end
+def user_plays_paper
+  @moves = ["Rock","Paper","Scissors"]
+
+
+
+  @comp_move = @moves.sample
+  render("game/play_paper.html.erb")
+end
+def user_plays_scissors
+  @moves = ["Rock","Paper","Scissors"]
+
+
+
+  @comp_move = @moves.sample
+  render("game/play_scissors.html.erb")
+end
+end
